2c51091f05bebb41235b79e3f2aae82ec0420dde
artemis-exo <vedaantbisht@gmail.com>
2026-07-16 22:20:19 +0530

    Time: 18 ms (21.99%), Space: 50.8 MB (10.72%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+       Map<String,List<String>>map=new HashMap<>();
+        for(int i=0; i<strs.length;i++){
+        int [] arr=new int[26];
+        for(int j=0; j<strs[i].length();j++){
+            arr[strs[i].charAt(j)-'a']++;
+        }
+        String key=Arrays.toString(arr);
+        map.putIfAbsent(key,new ArrayList<>());
+        map.get(key).add(strs[i]);
+            //System.out.println(Arrays.toString(arr));
+        }
+           
+        return new ArrayList<>(map.values());
+    }
+}
