3272a564a2682ee04409a7a6e86f0bc6365907d3
artemis-exo <vedaantbisht@gmail.com>
2026-07-19 00:21:30 +0530

    Refactor minimumEffortPath using Binary Search and DFS

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,40 +1,40 @@
-// // ** Using  Binary Search And DFS
-// class Solution {
-//     int [][] dir={{1,0},{0,1},{-1,0},{0,-1}};
-//     int m , n;
-//     public int minimumEffortPath(int[][] heights) {
-//         n=heights.length;
-//         m=heights[0].length;
-//         int low=0, high=1000000;
-//         while(low<=high){
-//             boolean [][] visit=new boolean[n][m];
-//             int mid=low+(high-low)/2;
-//             if(Effort(0,0,heights,mid,visit)){
-//                 high=mid-1;
-//             }
-//             else{
-//                 low=mid+1;
-//             }
-//         }
-//           return low;
-//     }
-//      public boolean Effort(int r, int c, int height[][], int mid,boolean[][]visit){
-//        if(r==n-1 && c==m-1)
-//        return true;
-//        visit[r][c]=true;
-//        for(int it[]:dir){
-//         int nr=it[0]+r;
-//         int nc=it[1]+c;
-//         if(nr>=0 && nc>=0 && nr<n && nc<m && !visit[nr][nc]){
-//             if(Math.abs(height[nr][nc]-height[r][c])<=mid){
-//                 if(Effort(nr,nc,height,mid,visit))
-//                 return true;
-//             }
-//         }
-//        }
-//        return false;
-//      }
-// }
+// ** Using  Binary Search And DFS
+class Solution {
+    int [][] dir={{1,0},{0,1},{-1,0},{0,-1}};
+    int m , n;
+    public int minimumEffortPath(int[][] heights) {
+        n=heights.length;
+        m=heights[0].length;
+        int low=0, high=1000000;
+        while(low<=high){
+            boolean [][] visit=new boolean[n][m];
+            int mid=low+(high-low)/2;
+            if(Effort(0,0,heights,mid,visit)){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+          return low;
+    }
+     public boolean Effort(int r, int c, int height[][], int mid,boolean[][]visit){
+       if(r==n-1 && c==m-1)
+       return true;
+       visit[r][c]=true;
+       for(int it[]:dir){
+        int nr=it[0]+r;
+        int nc=it[1]+c;
+        if(nr>=0 && nc>=0 && nr<n && nc<m && !visit[nr][nc]){
+            if(Math.abs(height[nr][nc]-height[r][c])<=mid){
+                if(Effort(nr,nc,height,mid,visit))
+                return true;
+            }
+        }
+       }
+       return false;
+     }
+}
 
 // ** Optimization Using Dijstra Algo
 class Solution {
