c003adab65a001bf9bb33018f03579a79d51a680
artemis-exo <vedaantbisht@gmail.com>
2026-07-20 23:20:17 +0530

    Time: 6 ms (55.4%), Space: 47.1 MB (64.37%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,14 +1,16 @@
 class Solution {
+         int n;
+        int col;
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         List<List<Integer>>res=new ArrayList<>();
-        int n=grid.length;
-        int m=grid[0].length;
-        k=k%(n*m);
-        int x=n*m;
+        n=grid.length;
+        col=grid[0].length;
+        k=k%(n*col);
+        int x=n*col;
         if(k>0) {
-            rev(grid,m,0,x-1);
-            rev(grid,m,0,k-1);
-            rev(grid,m,k,x-1);
+            rev(grid,0,x-1);
+            rev(grid,0,k-1);
+            rev(grid,k,x-1);
         }
         for(int i[]:grid){
             List<Integer>li=new ArrayList<>();
@@ -19,7 +21,7 @@ class Solution {
         }
         return res; 
     }
-    public void rev(int grid[][], int col, int i, int j){
+    public void rev(int grid[][], int i, int j){
         while(i<j){
             int temp=grid[i/col][i%col];
             grid[i/col][i%col]=grid[j/col][j%col];
